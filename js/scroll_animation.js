0761e964e6c19c77e89ba705bb1cedbc379c9c22
Gala Chernikova <gala.chernikova94@gmail.com>
2023-03-27 06:44:41 +0300

    Add animation

---
+++ js/scroll_animation.js
@@ -0,0 +1,108 @@
+// header
+ScrollReveal().reveal('.header-container',{ 
+    duration: 1000,
+    distance: '100px',
+    origin: 'top',
+    opacity: 0.2,
+});
+
+
+
+// main
+ScrollReveal().reveal('.main-container',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.features',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.create',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.feedback',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.brands',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.projects',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.proj-boxes',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.numbers',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.news',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.join',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+ScrollReveal().reveal('.join',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
+
+
+// footer
+ScrollReveal().reveal('.footer-container',{ 
+   
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    opacity: 0.2,
+});
